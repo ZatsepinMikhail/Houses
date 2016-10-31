fffeecc6ee9851f4e2cabc9d364bd1e6d26a417d
ZatsepinMikhail <salamence1996@gmail.com>
2016-10-31 15:25:30 +0300

    now able to connect database from c#

---
+++ DbCreater/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBConnect
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DBConnect db = new DBConnect("cities");
+            Console.WriteLine(db.Count("novokuznetsk"));
+            var result = db.Select("novokuznetsk", 2005, 2010);
+            Console.WriteLine(result.Count());
+            foreach (var coord in result)
+            {
+                Console.WriteLine("(" + coord.Item1 + ";" + coord.Item2 + ")");
+            }
+            string a;
+            a = Console.ReadLine();            
+        }
+    }
+}
