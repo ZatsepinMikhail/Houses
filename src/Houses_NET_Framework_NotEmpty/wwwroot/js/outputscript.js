c581982990a67f7c019a242142a8d2747e068aea
ZatsepinMikhail <salamence1996@gmail.com>
2016-11-28 15:00:20 +0600

    промежуточный коммит с оптимизированным отображением

--- src/Houses_NET_Framework_NotEmpty/wwwroot/js/outputscript.js
+++ src/Houses_NET_Framework_NotEmpty/wwwroot/js/outputscript.js
@@ -4,30 +4,9 @@ function updateTextInput() {
     var first = Math.floor(minYear + (maxYear - minYear) * (arr[0] / 100));
     var second = Math.floor(minYear + (maxYear - minYear) * (arr[1] / 100));
 
-    previousFromYear = currentFromYear;
-    previousToYear = currentToYear;
-
-    currentFromYear = first;
-    currentToYear = second;
+    newFromYear = first;
+    newToYear = second;
 
     document.getElementById('fromYear').innerHTML = first;
     document.getElementById('toYear').innerHTML = second;
-}
-
-function onClick() {
-    for (var year = previousFromYear; year < previousToYear; ++year) {
-        yearIndex = year - minYear;
-        for (var i = 0, l = markerArray[yearIndex].length; i < l; ++i) {
-            var marker = markerArray[yearIndex][i];
-            marker.setVisible(false);
-        }
-    }
-
-    for (var year = currentFromYear; year <= currentToYear; ++year) {
-        yearIndex = year - minYear;
-        for (var i = 0, l = markerArray[yearIndex].length; i < l; ++i) {
-            var marker = markerArray[yearIndex][i];
-            marker.setVisible(true);
-        }
-    }
 }
